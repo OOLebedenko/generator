e37cbef0b8a6885c2c2ead0f0b1c7d5e7af1ff68
OOLebedenko <oolebedenko@gmail.com>
2019-02-24 14:37:59 +0300

    add my_generator

---
+++ myrange.py
@@ -0,0 +1,30 @@
+def myrange(*args):
+    if len(args) == 1:
+        start, stop, step = 0, args[0], 1
+    if len(args) == 2:
+        start, stop, step = args[0], args[1], 1
+    if len(args) == 3:
+        start, stop, step = args[0], args[1], args[2]
+
+    if len(args) == 0:
+        raise TypeError("range expected 1 arguments, got 0")
+    if len(args) > 3:
+        raise TypeError("range expected at most 3 arguments, got {}".format(len(args)))
+
+    try:
+        start, stop, step = int(start), int(stop), int(step)
+    except ValueError:
+        raise TypeError("an integer is required")
+
+    if step > 0:
+        while start < stop:
+            yield start
+            start += step
+    elif step < 0:
+        while start > stop:
+            yield start
+            start += step
+    else:
+        raise ValueError('range() arg 3 must not be zero')
+
+
---
+++ tests/test_myrange.py
@@ -0,0 +1,36 @@
+import unittest
+from tests.hw01.myrange import myrange
+
+
+class TestIntegrator(unittest.TestCase):
+
+    def test_one_parameter(self):
+        self.assertEqual(list(myrange(4)), list(range(4)))
+        self.assertEqual(list(myrange(-4)), list(range(-4)))
+
+    def test_two_parameters(self):
+        self.assertEqual(list(myrange(1, 3)), list(range(1, 3)))
+        self.assertEqual(list(myrange(-1, -3)), list(range(-1, -3)))
+        self.assertEqual(list(myrange(-3, -1)), list(range(-3, -1)))
+
+    def test_three_parameters(self):
+        self.assertEqual(list(myrange(1, 5, 2)), list(range(1, 5, 2)))
+        self.assertEqual(list(myrange(-1, 5, 2)), list(range(-1, 5, 2)))
+        self.assertEqual(list(myrange(-1, -5, 2)), list(range(-1, -5, 2)))
+        self.assertEqual(list(myrange(-1, -5, -2)), list(range(-1, -5, -2)))
+        self.assertEqual(list(myrange(-1, 5, -2)), list(range(-1, 5, -2)))
+        self.assertEqual(list(myrange(1, -5, -2)), list(range(1, -5, -2)))
+
+    def test_given_zero_step(self):
+        self.assertRaises(ValueError, list, myrange(1, -5, 0))
+
+    def test_no_integer(self):
+        self.assertRaises(TypeError, myrange(""))
+
+    def test_given_argument(self):
+        self.assertRaises(TypeError, myrange())
+        self.assertRaises(TypeError, myrange())
+
+
+
+
