e07f0129b41a1445ef1942fff9ea7afad20c78bc
OOLebedenko <oolebedenko@gmail.com>
2019-03-06 09:33:39 +0300

    rename trace function

---
+++ trace_function.py
@@ -0,0 +1,19 @@
+import sys
+
+
+def tracefunc(frame, event, arg):
+    if event == 'return':
+        print(f'function: {frame.f_code.co_name},',
+              f'local_vars: {list(frame.f_locals.keys())}')
+    return tracefunc
+
+
+def foo():
+    friends = ["Bob", "Tom"]
+    for f in friends:
+        print(f"Hi {f}")
+    return len(friends)
+
+
+sys.settrace(tracefunc)
+number_friends = foo()
