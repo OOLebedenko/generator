428a6761a6d584b1a260e36b552e335190cd0d30
OOLebedenko <oolebedenko@gmail.com>
2019-03-12 22:35:31 +0300

    Correct with Python 3.6+

--- trace_function.py
+++ trace_function.py
@@ -2,7 +2,7 @@ import sys
 
 
 def tracefunc(frame, event, arg):
-    if event == 'return':
+    if event == 'return' and frame.f_locals:
         print(f'function: {frame.f_code.co_name},',
               f'local_vars: {list(frame.f_locals.keys())}')
     return tracefunc
